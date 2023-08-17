7f932d8ea413ead773b8fce0f5d1692dc5b8e2be
alenapech <alenaryzhova@inbox.ru>
2023-08-18 01:10:14 +0600

    Scripts to start all kind of scenarios/suites have been added (for Unix)

---
+++ app-startup/uni-webkit-suite.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+JAVA_HOME="/usr/lib/jvm/graalvm-ce-java11-21.3.0"
+$JAVA_HOME/bin/java -cp "*:test-classes:lib/*" org.junit.platform.console.ConsoleLauncher --select-class org.alenapech.qadiploma.suite.WebkitSuite
--- app-startup/win-chromium-suite.bat
+++ app-startup/win-chromium-suite.bat
@@ -1,5 +1,5 @@
 @echo off
 
-set JAVA_HOME="C:\Program Files\Java\graalvm-ce-java11-21.3.0\bin"
-%JAVA_HOME%\java -cp "*;test-classes;lib\*" org.junit.platform.console.ConsoleLauncher --select-class org.alenapech.qadiploma.suite.ChromiumSuite
+set JAVA_HOME="C:\Program Files\Java\graalvm-ce-java11-21.3.0"
+%JAVA_HOME%\bin\java -cp "*;test-classes;lib\*" org.junit.platform.console.ConsoleLauncher --select-class org.alenapech.qadiploma.suite.ChromiumSuite
 pause
